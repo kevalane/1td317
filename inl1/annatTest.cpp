731739a8853d0c47fb09291ff6e5ff16b8113b24
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-15 17:58:42 +0200

    done inl 1

---
+++ inl1/annatTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int a;
+  int b = 12;
+  a = 0; 
+
+  cout << "a = " << a << endl;
+  cout << a+b << '\n' << "a-b = " << a-b << endl;
+
+  return 0;
+}
---
+++ inl1/fungerandeTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  cout << "Grattis! Du har kompilerat och testk�rt den f�rsta programfilen i " 
+       << endl
+       << "kursen. Nu vet du att du har lyckats med installationen och att " 
+       << endl
+       << "kompilatorn fungerar. G� f�rslagsvis vidare till n�sta punkt p� "
+       << endl 
+       << "OU1, d�r man ska kompilera lite felaktig kod." << endl;
+
+  return 0;
+}
