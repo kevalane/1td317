776634fabf905511b0d7fece80a5697eb8aef1e1
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-15 20:32:08 +0200

    added test printouts and comments

--- inl2/main.cpp
+++ inl2/main.cpp
@@ -16,7 +16,6 @@ int main() {
     cout << "Startnummer? ";
     cin >> startNumber;
 
-    // main input loop
     while (startNumber > 0) {
         // Get start and end time
         cout << "Starttid? ";
@@ -24,10 +23,13 @@ int main() {
         cout << "Måltid? ";
         cin >> endHours >> endMinutes >> endSeconds;
 
+        // convert times to seconds with help function
         int startTime = convertToSeconds(startHours, startMinutes, startSeconds);
         int endTime = convertToSeconds(endHours, endMinutes, endSeconds);
 
         if (endTime <= startTime) endTime += 24*60*60; // add 24 hours in seconds to endTime if past midnight
+        
+        // calc diff and update best if necessary
         int diff = endTime - startTime;
         if (diff < bestDiff) {
             bestDiff = diff;
@@ -38,14 +40,21 @@ int main() {
         numberOfRunners++;
         cout << "Startnummer? ";
         cin >> startNumber;
-    }
+    } // end while (startNumber > 0)
     
     printEnding(numberOfRunners, bestStartNumber, bestDiff);
     return 0;
 }
 
+/**
+ * @brief Prints ending in accordance with specified instructions.
+ * Two cases, if 0 competitors or not
+ * @param number_of_runners how many people were competing
+ * @param winner_number the startNumber of the winner
+ * @param winner_time the time between startTime and endTime in seconds for the winner
+ */
 void printEnding(int number_of_runners, int winner_number, int winner_time) {
-    // let's convert winnertime to hh-mm-ss
+    // convert winnertime to hh-mm-ss
     int hours, seconds, minutes;
     seconds = winner_time;
     minutes = winner_time / 60;
@@ -63,6 +72,14 @@ void printEnding(int number_of_runners, int winner_number, int winner_time) {
     cout << "Programmet avslutas" << endl;
 }
 
+/**
+ * @brief Converts hours, minutes and seconds to only seconds
+ * 
+ * @param hours number of hours to convert, e.g. 14
+ * @param minutes number of minutes to convert, e.g. 52
+ * @param seconds number of seconds to convert, e.g. 32
+ * @return int number of seconds from hours, minutes & seconds. Our e.g. would return 14*3600 + 52*60 + 32
+ */
 int convertToSeconds(int hours, int minutes, int seconds) {
     return (hours * 60 * 60 + minutes * 60 + seconds);
 }
