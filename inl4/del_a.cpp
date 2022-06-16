eaa065e16a4b26d457a4a8ca20b68cf5ffd1b633
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-17 00:28:38 +0200

    added part a shell

---
+++ inl4/del_a.cpp
@@ -0,0 +1,53 @@
+//
+// Programskal till inlämningsuppgift 4 - deluppgift A
+//
+// Hanterar fallet med 26 bokstäver A-Z
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <cmath>
+using namespace std;
+
+
+// Använd gärna denna globala konstant i programmet
+// i stället för värdet 26
+const int ANTAL_BOKSTAVER = 26;  //A-Z
+// Globala variabler är ej tillåtna
+
+//--------------------------------------------------------
+// Funktionsdeklarationer:
+// Använd exakt de funktionsnamn som finns nedan
+
+// Funktionen berakna_histogram_abs
+
+// Funktionen skriv_histogram_abs
+
+//--------------------------------------------------------
+// Huvudprogram:
+
+int main()
+{
+  // Deklarera variabler
+  ...
+
+  // Läs in en rad med text från tangentbordet
+  ...
+
+  // Anropa funktionen berakna_histogram_abs som beräknar histogrammet
+  // och antalet bokstäver.  
+  ...
+ 
+
+  // Anropa funktionen skriv_histogram_abs som skriver ut histogrammet
+  ...
+
+  return 0;
+}
+
+//--------------------------------------------------------
+// Funktionsdefinitioner:
+...
+
+
+// Redovisning av uppgiften i ord. 
