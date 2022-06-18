64214a23c32e50415c1535bcec6088a375f0508b
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-19 00:00:38 +0200

    implemented funcs for part a
    :

--- inl5/Text.cpp
+++ inl5/Text.cpp
@@ -5,10 +5,9 @@
 using namespace std;
 
 Text::Text() {
-    cout << "Hit" << endl;
-    // text = "";
-    // abs_histogram = {0};
-    // letter_count = 0;
+    text = "";
+    for (int i = 0; i < ANTAL_BOKSTAVER; i++) abs_histogram[i] = 0;
+    letter_count = 0;
 }
 
 void Text::setText(const string &text) {
@@ -16,9 +15,23 @@ void Text::setText(const string &text) {
 }
 
 bool Text::calcHistogramAbs() {
+    if (this->text.size() == 0) return false;
+    for (int i = 0; i < this->text.size(); i++) {
+        if ((int) text[i] > 90) this->text[i] = this->text[i] - 32; // convert all to capital letters
+        int index = (int) this->text[i] - 65; // shift index 65, A = 0, B = 1 etc
+        if (index < ANTAL_BOKSTAVER && index >= 0) {
+            this->abs_histogram[index]++; 
+            this->letter_count++;
+        }
+    }
     return true;
 }
 
 void Text::printHistogramAbs() {
-    cout << text << endl;
+    cout << "\nResultat för bokstäverna A-Z\n" << endl;
+    cout << "Totala antalet bokstäver: " << this->letter_count << endl;
+    cout << "Bokstavsfördelning:\n" << endl;
+    for (int i = 0; i < ANTAL_BOKSTAVER; ++i) {
+        cout << (char) (i + 65) << ": " << this->abs_histogram[i] << endl;
+    }
 }
