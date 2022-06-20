28c076a3848a257c22b0dc1404e4545e9da93145
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-20 22:43:26 +0200

    added friendExists

--- inl6/Transaction.cpp
+++ inl6/Transaction.cpp
@@ -20,6 +20,15 @@ double Transaction::getAmount() {
     return this->amount;
 }
 
+int Transaction::getNumerOfFriends() {
+    return this->number_of_friends;
+}
 
+bool Transaction::friendExists(const string &name) {
+    for (int i = 0; i < MAX_PERSONS; i++) {
+        if (this->friends[i] == name) return true;
+    }
+    return false;
+} 
 
 Transaction::~Transaction() {}
