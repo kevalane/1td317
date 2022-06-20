589dcdd0172d64e72b116e3480d6a44d37ece1f7
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-20 23:17:54 +0200

    read working

--- inl6/main.cpp
+++ inl6/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <fstream>
+#include <cmath>
 
 #include "Transaction.h"
 
@@ -6,5 +10,9 @@ using namespace std;
 
 int main() {
     Transaction t = Transaction();
+    string file_name = "txt/resa.txt";
+    ifstream fin(file_name.c_str());
+    t.read(fin);
+    cout << t.getName() << endl;
     return 0;
 }
