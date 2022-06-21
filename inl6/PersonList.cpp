00ec5eac55fb495ada3ad2d91210cbe4a9f34046
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-22 00:08:04 +0200

    first implementation of PersonList, requires testing

--- inl6/PersonList.cpp
+++ inl6/PersonList.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include "constants.h"
 #include "Person.h"
+#include "PersonList.h"
+
+using namespace std;
 
 PersonList::PersonList()
 : person_count(0)
@@ -16,7 +19,38 @@ void PersonList::addPerson(Person newPerson)
     person_count++;
 }
 
-bool PersonList::personExists(const std::string &name)
+PersonList::~PersonList()
+{
+}
+
+void PersonList::writeAndFix(ostream &os)
+{
+    // print the persons
+    for (int i = 0; i < person_count; i++) {
+        persons[i].write(os);
+    }
+    cout << this->sumOwed() << "==" << this->sumPayed() << endl;
+}
+
+double PersonList::sumOwed()
+{
+    double sum = 0.0;
+    for (int i = 0; i < person_count; i++) {
+        sum += persons[i].getOwed();
+    }
+    return sum;
+}
+
+double PersonList::sumPayed()
+{
+    double sum = 0.0;
+    for (int i = 0; i < person_count; i++) {
+        sum += persons[i].getPayed();
+    }
+    return sum;
+}
+
+bool PersonList::personExists(const string &name)
 {
     for (int i = 0; i < person_count; i++) {
         if (persons[i].getName() == name) {
