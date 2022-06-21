7384374831d6b9beadb8cefcc2adc28dd9d523e8
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-22 01:20:06 +0200

    copied personPayed from inl6

---
+++ inl6/TransactionList.cpp
@@ -0,0 +1,47 @@
+#include "TransactionList.h"
+#include "Transaction.h"
+
+using namespace std;
+
+TransactionList::TransactionList()
+: transaction_count(0)
+{
+    for (int i = 0; i < MAX_TRANSACTIONS; i++) {
+        transactions[i] = Transaction();
+    }
+}
+
+TransactionList::~TransactionList()
+{
+}
+
+void TransactionList::read(istream &is) {
+
+}
+
+void TransactionList::write(ostream &os) {
+
+}
+
+void TransactionList:addTransaction(Transaction &t) {
+    this->transactions[transaction_count] = t;
+    transaction_count++;
+}
+
+double TransactionList::totalCost() {
+    double total = 0;
+    for (int i = 0; i < transaction_count; i++) {
+        total += transactions[i].getAmount();
+    }
+    return total;
+}
+
+double TransactionList::personPayed(const string &name) {
+    double total = 0;
+    for (int i = 0; i < transaction_count; i++) {
+        if (transactions[i].getName() == name) {
+            total += transactions[i].getAmount() * (1.0 - 1.0/transactions[i].getNumerOfFriends() + 1.0);
+        }
+    }
+    return total;
+}
