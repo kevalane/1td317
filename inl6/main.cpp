f0f59aac8c8a95a39fc915e5bf792c3203395018
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-22 21:51:56 +0200

    made test report for part a

--- inl6/main.cpp
+++ inl6/main.cpp
@@ -16,7 +16,7 @@ int main()
   cout << "Startar med att läsa från en fil." << endl;
 
   TransactionList transaktioner;
-  std::ifstream     is("txt/test2.txt");
+  std::ifstream     is("txt/resa.txt");
   transaktioner.read(is);
 
   int operation = 1;
