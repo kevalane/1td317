fa04b23f9cf7b661414be69c84f7c90042b46e30
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-08-28 13:43:55 +0200

    solved number 3

--- tenta/main.cpp
+++ tenta/main.cpp
@@ -9,6 +9,8 @@ int main() {
     ofstream fout;
     int n1, n2;
     string s1, s2;
+    int place1 = 0;
+    int place2 = 0;
 
     fin1.open("temperatur1.dat");
     fin2.open("temperatur2.dat");
@@ -16,16 +18,35 @@ int main() {
 
     while(fin1 >> n1 && fin2 >> n2)
     {
-    if(n1 < n2)
-        fout << n1 << '\n';
-    else
-        fout << n2 << '\n';
+        if(n1 < n2)
+        { 
+            double temp;
+            fin1 >> temp;
+            fout << n1 << " " << temp << endl;
+            fin2.seekg(place2);
+        } else {
+            double temp;
+            fin2 >> temp;
+            fout << n2 << " " << temp << endl;
+            fin1.seekg(place1);
+        }
+
+        place1 = fin1.tellg();
+        place2 = fin2.tellg();
     }
 
     // This loop will only execute if fin1 is not at eof().
-    while(fin1 >> n1) fout << n1 << '\n';
+    while(fin1 >> n1) {
+        double temp;
+        fin1 >> temp;
+        fout << n1 << " " << temp << endl;
+    }
     // This loop will only execute if fin2 is not at eof().
-    while(fin2 >> n2) fout << n2 << '\n';
+    while(fin2 >> n2) {
+        double temp;
+        fin2 >> temp;
+        fout << n2 << " " << temp << endl;
+    }
 
     fin1.close();
     fin2.close();
